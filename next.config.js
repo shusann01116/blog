84039e0b83c625f213573196b9ca98adb75aa5cd
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-11-25 18:52:14 +0900

    Update info

--- next.config.js
+++ next.config.js
@@ -3,7 +3,20 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.jsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      }
+    ]
+  },
+  i18n: {
+    locales: ["en-US", "ja-JP"],
+    defaultLocale: "en-US",
+  },
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,56 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+
 export default {
-  logo: <span>shusann's showcase</span>,
+  logo: (
+    <>
+      <span style={{ merginLeft: "0.5rem", fontWeight: 800 }}>shusann&apos;s showcase</span>
+    </>
+  ),
   project: {
     link: "https://github.com/shusann01116/showcase",
   },
-  // ... other theme options
+  docsRepositoryBase: "https://github.com/shusann01116/showcase/tree/main",
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s | shusann's showcase",
+        openGraph: {
+          title: "shusann's showcase",
+        },
+      }
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url = `https://showcase.shusann.dev${asPath === "/" ? "" : asPath}`;
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:site_name" content="shusann's showcase" />
+        <meta property="og:title" content={frontMatter.title} />
+        <meta property="og:description" content={frontMatter.description} />
+        <meta property="og:image" content={`https://showcase.shusann.dev/og/${locale || defaultLocale}.png`} />
+      </>
+    )
+  },
+  i18n: [
+    { locale: "en-US", text: "English" },
+    { locale: "ja-JP", text: "日本語" },
+  ],
+  footer: {
+    text: (
+      <>
+        <span>&copy; {new Date().getFullYear()} shusann</span>
+        <a href="https://showcase.shusann.dev" target="_blank" rel="noopener noreferrer" style={{ marginLeft: "1rem" }}>
+          showcase.shusann.dev
+        </a>
+      </>
+    )
+  },
+  faviconGlyph: "📦",
 };
