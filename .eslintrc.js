7ec6a6ad5d47812b0819c3b41fb7a6dae45f1c4c
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-01-21 11:58:42 +0900

    Add gtag

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:mdx/recommended",
-    "plugin:@next/next/recommended",
-  ],
-  settings: {
-    "mdx/code-blocks": true,
-  },
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "react"],
-  rules: {},
-};
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+    }
+}
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,7 +1,17 @@
-import { Noto_Sans_JP } from "@next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import React from "react";
-const notoSansJP400 = Noto_Sans_JP({ weight: '400',display:'swap',preload:false, });
+const notoSansJP400 = Noto_Sans_JP({
+  weight: "400",
+  display: "swap",
+  preload: false,
+});
 
 export default function Main({ children }) {
-  return <main className={notoSansJP400.className}>{children}</main>;
+  return (
+    <>
+      <GoogleAnalytics gaId="G-2NJX07FBDF" />
+      <main className={notoSansJP400.className}>{children}</main>;
+    </>
+  );
 }
