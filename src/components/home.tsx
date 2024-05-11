aecdd8f42d7f570654815f22b4636f5b5c2cbc90
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-05-12 01:29:52 +0900

    Give margine for mobile

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -13,7 +13,7 @@ export function Home() {
   };
 
   return (
-    <div className="flex flex-col grow items-center justify-center h-screen">
+    <div className="flex flex-col grow items-center justify-center h-screen mx-4">
       <h1 className="text-4xl font-bold mb-6">Welcome to shusann's Blog</h1>
       <p className="text-xl mb-8">
         Dive into a world of thoughts and discoveries.
